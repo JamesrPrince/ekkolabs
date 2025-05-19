e7faa778e3152cd79c376fe61e5f5f5d237ec6c4
JamesrPrince <jameslira36@yahoo.com>
2025-05-19 23:39:24 +0200

    Vercel loading issue v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { Switch, Route } from "wouter";
+import { Analytics } from "@vercel/analytics/next";
+
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
